12222ea59c73a2c47b51dd15fc4a50df942864b2
James Hovet <jrhovet@gmail.com>
2021-10-28 09:25:56 -0500

    make download code more robust for anyone who wants to build

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 import lyricsgenius
 import time
 import os
+import pathlib
+
 genius = lyricsgenius.Genius("ihSSN2XhKiYdy9s-fk13_MTSpEXjC9EZr__qOC4n9XDUTuOarb6EYJHOMp5arv7-DE3PN0h8P9w8L4o9o6Coqw")
 
 album_titles = [
@@ -18,6 +20,7 @@ album_titles = [
    'Let It Be',
 ]
 
+pathlib.Path('./data_source').mkdir(exist_ok=True)
 os.chdir("./data_source")
 current_dir_files = os.listdir()
 
@@ -26,8 +29,9 @@ for album_title in album_titles:
         print("Saving", album_title)
         album = genius.search_album(album_title, "The Beatles")
         album.save_lyrics(album_title)
-        time.sleep(10)
+        time.sleep(10) ## rate limiting 
 
 
+## Sgt. Pepper wasn't showing up in search, so I hacked it in
 album = genius.search_album(album_id=11039, artist="The Beatles")
 album.save_lyrics("Sgt.PeppersLonelyHeartsClubBand")
